6a63a2844a08355d03b0473ee4bc3c868a8c56e6
Ahmad <hereahmad@gmail.com>
2024-11-05 21:14:43 +0300

    Logo SVG dark and light added

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,5 +1,9 @@
+"use client";
+import { useTheme } from "next-themes";
 import Image from "next/image";
 
 export default function Logo({ width = 30, height = 30, alt = "Logo", ...props }) {
-  return <Image src='/logo.svg' alt={alt} width={width} height={height} {...props} />;
+  const { theme } = useTheme();
+  const source = theme === "light" ? "/logo.svg" : "/logo-dark.svg";
+  return <Image src={source} alt={alt} width={width} height={height} {...props} />;
 }
